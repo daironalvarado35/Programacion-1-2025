a6214a8c2086d1c6969c083290d23ccdca048dbd
daironalvarado35 <daironalvarado35@gmail.com>
2025-05-05 19:37:31 -0300

    add  other class

---
+++ CLASE 050525/ejercicio1.py
@@ -0,0 +1,21 @@
+"""
+def sumar (a: int, b: int)-> int  :
+    return a +b
+
+resultado = sumar (3,5)
+print (resultado)
+
+"""
+"""
+def dividir (a: int, b: int) -> int :
+    return a / b
+resultado = dividir (100 , 2)
+print (resultado)
+"""
+
+def sumar (a: int, b: int)-> int  :
+    return int (a +b)
+
+resultado = sumar (25.5 , 50.1)
+print (resultado)
+
